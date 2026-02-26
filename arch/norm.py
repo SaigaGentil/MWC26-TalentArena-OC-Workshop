64259a55c90c24cbfcd60aa78f5d1036988afcf7
bden <bohdan.denysenko@openchip.com>
2026-02-26 14:21:46 +0100

    coded LayerNorm

---
+++ arch/norm.py
@@ -0,0 +1,17 @@
+from torch import nn
+import torch
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x: torch.Tensor):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+
+        norm_x = (x - mean) / torch.sqrt(var + self.eps) # Normalize to zero mean and variance of 1
+        return self.scale * norm_x + self.shift
